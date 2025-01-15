0fd3f5c452e2c2d31080c9c82bd6ebdab511e8aa
nomad777 <apatrickl@icloud.com>
2025-01-14 20:08:23 -0500

    Create multidimensionalarr.c

---
+++ multidimensionalarr.c
@@ -0,0 +1,64 @@
+// This is a program I built which utilizes a multidimensional array to handle student grades in a gradebook
+// Initially I thought of this program as a 3 dimensional excel file with data x(number of classes), y(number of assignments), z(number of students) all equal to (3,3,3)
+// You can build this multidimensional array to any dimensions or parameters youd like
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(){
+
+	int numclasses;
+	int numassignments;
+	int numstudents;
+
+	scanf("%d %d %d", &numclasses, &numassignments, &numstudents);
+	
+	int data[numclasses][numassignments][numstudents];
+	
+	for(int c = 0 ; c < numclasses ; c++){
+		for(int a = 0 ; a < numassignments ; a++){
+			for(int s = 0 ; s < numstudents ; s++){
+				scanf("%d", &data[c][a][s]); 
+			}
+		}
+	}
+	
+	for(int c = 0 ; c < numclasses ; c++){
+
+		float totalov = 0;
+	
+		for(int a = 0 ; a < numassignments ; a++){
+	
+			float total = 0;
+	
+			for(int s = 0 ; s < numstudents ; s++){
+				
+				 total += data[c][a][s];
+
+							
+			}
+	
+		
+		float average = total / numstudents;
+		
+		printf("%.2f " , average);
+
+
+		
+		}
+	printf("\n");
+	}
+	for(int s = 0 ; s < numstudents ; s++){
+		int total = 0;
+		for(int a = 0 ; a < numassignments ; a++){
+			for(int c = 0 ; c < numclasses ; c++){
+				total += data[c][a][s];
+			}
+		}
+	printf("%d ", total);
+	}
+
+	return 0;
+}
